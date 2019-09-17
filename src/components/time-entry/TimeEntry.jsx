e8de3ef8462f9b88206321198e265b173a201f20
Dylan Jongbloed <dylan.jongbloed@humanoids.nl>
2019-09-17 13:34:03 +0200

    Add duration element to Time Entry & fix previous comments

--- src/components/time-entry/TimeEntry.jsx
+++ src/components/time-entry/TimeEntry.jsx
@@ -5,7 +5,13 @@ import styles from './TimeEntry.module.css';
 const TimeEntry = () => (
   <div className={styles.timeEntry}>
     <span className={styles.client}>Port of Rotterdam</span>
-    <span className={styles.timeStamp}>09:00-17:00</span>
+    <div className={styles.timeStamp}>
+      <span>09:00 - 17:00</span>
+      <span className={styles.duration}>
+        <br />
+        08:00:00
+      </span>
+    </div>
   </div>
 );
 
