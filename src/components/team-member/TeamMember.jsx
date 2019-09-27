0648065996ceefc4a09b126c83122c5e261e5bef
Dylan Jongbloed <dylan.jongbloed@humanoids.nl>
2019-09-27 12:28:26 +0200

    Build Team Members page drop down employer info in desktop & mobile

--- src/components/team-member/TeamMember.jsx
+++ src/components/team-member/TeamMember.jsx
@@ -1,30 +1,72 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import employeeAvatar from '../../../static/pictures/picture-tnt.jpg';
-import styles from './TeamMember.module.css';
 import MenuDownIcon from '../../assets/icons/icon-arrow-down.svg';
+import styles from './TeamMember.module.css';
+import teamMemberAvatar from '../../../static/pictures/picture-tnt.jpg';
 
 const TeamMember = () => {
+  const [isTeamMemberActive, setTeamMemberActivity] = useState(false);
+
+  const toggleTeamMemberActivity = () => {
+    setTeamMemberActivity(!isTeamMemberActive);
+  };
+
   return (
-    <div className={styles.teamMemberActive}>
-      <img className={styles.employeeAvatar} src={employeeAvatar} alt="Logo" />
-      <div className={styles.employeeTitle}>
-        <span className={styles.employeeName}>Antje Adriaens</span>
-        <span className={styles.employeeFunction}>UX Designer</span>
+    <div
+      className={`${
+        isTeamMemberActive
+          ? styles.teamMemberEntryActive
+          : styles.teamMemberEntry
+      }`}
+    >
+      <div className={styles.teamMemberBasicInfo}>
+        <img
+          className={styles.teamMemberAvatar}
+          src={teamMemberAvatar}
+          alt="Logo"
+        />
+        <div className={styles.teamMemberTitle}>
+          <span className={styles.teamMemberName}>Antje Adriaens</span>
+          <span className={styles.teamMemberFunction}>UX Designer</span>
+        </div>
+        <div className={styles.extraInfoDesktop}>
+          <div className={styles.teamMemberNumber}>
+            <span className={styles.extraInfoMainDesktop}>HUM_001</span>
+            <span className={styles.extraInfoSubDesktop}>Employee number</span>
+          </div>
+          <div className={styles.extraEmployerInfo}>
+            <span className={styles.extraInfoMainDesktop}>Hike One</span>
+            <span className={styles.extraInfoSubDesktop}>Current Employer</span>
+          </div>
+          <div className={styles.extraStartingDate}>
+            <span className={styles.extraInfoMainDesktop}>February 2018</span>
+            <span className={styles.extraInfoSubDesktop}>Starting date</span>
+          </div>
+        </div>
+        <MenuDownIcon
+          className={styles.menuDownIcon}
+          onClick={toggleTeamMemberActivity}
+        />
       </div>
-      <div className={styles.extraInfo}>
-        <span className={styles.extraInfoMain}>HUM_001</span>
-        <span className={styles.extraInfoSub}>Employee number</span>
+      <div
+        className={`${
+          isTeamMemberActive ? styles.extraInfoActive : styles.extraInfo
+        }`}
+      >
+        <div className={styles.extraInfoHeader}>
+          <span className={styles.extraInfoHeaderText}>
+            Detailed information about Antje
+          </span>
+        </div>
+        <div className={styles.extraEmployerInfo}>
+          <span className={styles.extraInfoMain}>Hike One</span>
+          <span className={styles.extraInfoSub}>Current Employer</span>
+        </div>
+        <div className={styles.extraStartingDate}>
+          <span className={styles.extraInfoMain}>February 2018</span>
+          <span className={styles.extraInfoSub}>Starting date</span>
+        </div>
       </div>
-      <div className={styles.extraInfo}>
-        <span className={styles.extraInfoMain}>Hike One</span>
-        <span className={styles.extraInfoSub}>Current Employer</span>
-      </div>
-      <div className={styles.extraInfo}>
-        <span className={styles.extraInfoMain}>February 2018</span>
-        <span className={styles.extraInfoSub}>Starting date</span>
-      </div>
-      <MenuDownIcon className={styles.menuDownIcon} />
     </div>
   );
 };
--- src/components/team-members/TeamMembers.jsx
+++ src/components/team-members/TeamMembers.jsx
@@ -5,13 +5,26 @@ import styles from './TeamMembers.module.css';
 
 const TeamMembers = () => {
   return (
-    <div className={styles.teamMembersContainer}>
-      <button type="button" className={styles.newHumanoidButton}>
-        + New Humanoid
-      </button>
-      <TeamMember className={styles.teamMember} />
-      <TeamMember className={styles.teamMember} />
-      <TeamMember className={styles.teamMember} />
+    <div>
+      <div className={styles.headerTeamMembers}>
+        <span className={styles.headerTeamMembersText}> All Humanoids </span>
+        <button type="button" className={styles.newHumanoidButtonDesktop}>
+          + New Humanoid
+        </button>
+        <select type="button" className={styles.sortTeamMembersSelector}>
+          <option value="" disabled selected>
+            Sort by:
+          </option>
+          <option value="hurr">Durr</option>
+        </select>
+      </div>
+      <div className={styles.teamMembersContainer}>
+        <button type="button" className={styles.newHumanoidButton}>
+          + New Humanoid
+        </button>
+        <TeamMember className={styles.teamMember} />
+        <TeamMember className={styles.teamMember} />
+      </div>
     </div>
   );
 };
