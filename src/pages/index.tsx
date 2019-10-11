d4bb3d8484823c74a180774ef5dfbccf978027d1
Dylan Jongbloed <dylan.jongbloed@humanoids.nl>
2019-10-11 17:19:07 +0200

    Finalize typechecking of all time entry components w/ successful compile but w/ browser error

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,9 +2,9 @@ import React from 'react';
 
 import Header from '../components/header/Header';
 import Layout from '../components/layout/Layout';
-import TimeEntries from '../components/time-entries/';
+import TimeEntries from '../components/time-entries';
 
-export default () => (
+export default (): React.ReactElement => (
   <Layout>
     <Header />
     <TimeEntries />
