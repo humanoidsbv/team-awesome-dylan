6eb6dd86c547c9a3ec2474395632abb7f997d8c0
Dylan Jongbloed <dylan.jongbloed@humanoids.nl>
2019-10-17 12:06:54 +0200

    Create timeentryheading test & add jest as environment to eslint

---
+++ src/components/time-entry-heading/__tests__/TimeEntryHeading.test.tsx
@@ -0,0 +1,13 @@
+import * as React from 'react';
+import { shallow } from 'enzyme';
+
+import TimeEntryHeading from '../TimeEntryHeading';
+
+it('matches snapshot', () => {
+  const timeEntryHeading = shallow(
+    <TimeEntryHeading
+    startTimestamp='test'
+    />
+  );
+  expect(timeEntryHeading).toMatchSnapshot();
+});
