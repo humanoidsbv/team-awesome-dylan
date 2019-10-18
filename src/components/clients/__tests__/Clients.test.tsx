04792d84c02c746aa420f9860fd5ba91af3bb27a
Dylan Jongbloed <dylan.jongbloed@humanoids.nl>
2019-10-18 11:12:07 +0200

    Alphabetize code for tests

--- src/components/clients/__tests__/Clients.test.tsx
+++ src/components/clients/__tests__/Clients.test.tsx
@@ -3,15 +3,17 @@ import { mount, shallow } from 'enzyme';
 
 import Clients from '../Clients';
 
+const mockedClients = [{
+  branch:'test',
+  clientName:'test',
+  id: 123
+}];
+
 describe('Clients', () => {
   it('matches snapshot', () => {
     const clients = shallow(
       <Clients
-        clients ={[{
-          branch:'test',
-          clientName:'test',
-          id: 123
-        }]}
+        clients ={mockedClients}
         createClient={({}) => null}
         fetchClients={()=> null}
       />
@@ -22,20 +24,14 @@ describe('Clients', () => {
   it('client entry form only shows when clicking the button', () => {
     const clients = mount(
       <Clients
-        clients ={[{
-          branch:'test',
-          clientName:'test',
-          id: 123
-        }]}
+        clients ={mockedClients}
         createClient={({}) => null}
         fetchClients={()=> null}
       />
     );
-    
+  
     expect(clients.find('.clientForm').prop('className').includes('clientFormHide')).toBe(true);
-
     clients.find('.newClientButton').simulate('click');
-
     expect(clients.find('.clientForm').prop('className').includes('clientFormHide')).not.toBe(true);
   })
 });
--- src/components/time-entries/__tests__/TimeEntries.test.tsx
+++ src/components/time-entries/__tests__/TimeEntries.test.tsx
@@ -3,28 +3,32 @@ import { shallow } from 'enzyme';
 
 import TimeEntries from '../TimeEntries';
 
+const mockedTimeEntries = [{
+  client:{
+    branch: '123',
+    clientName: '123',
+    contactPerson: 'test',
+    contactPhoneNumber: 'test',
+    id: 123,
+    locality: '123',
+    postalCode: '123'
+  },
+  id: 123,
+  startTimestamp: 'bla',
+  stopTimestamp: 'bla'
+}]
+
+const mockedClients = [{
+  branch:'test',
+  clientName:'test',
+  id: 123
+}];
+
 it('matches snapshot', () => {
   const timeEntries = shallow(
     <TimeEntries
-      clients ={[{
-        branch:'test',
-        clientName:'test',
-        id: 123
-      }]}
-      timeEntries={[{
-        id: 123,
-        startTimestamp: 'bla',
-        stopTimestamp: 'bla',
-        client:{
-          branch: '123',
-          clientName: '123',
-          id: 123,
-          locality: '123',
-          postalCode: '123',
-          contactPerson: 'test',
-          contactPhoneNumber: 'test'
-        }
-      }]}
+      clients ={mockedClients}
+      timeEntries={mockedTimeEntries}
       createTimeEntry={({}) => null}
       deleteTimeEntry={(id: number) => null}
       fetchTimeEntries={()=> null}
